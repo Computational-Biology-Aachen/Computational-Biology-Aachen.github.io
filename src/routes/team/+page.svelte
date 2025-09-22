26917750662f64f677a805d17153e5cce0daf70a
Marvin van Aalst <marvin.vanaalst@gmail.com>
2025-09-22 14:56:38 +0200

    dev: chg: move past theses

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-	import AlumniTable from '$lib/AlumiTable.svelte';
 	import Person from '$lib/cards/Person.svelte';
 	import GridPerson from '$lib/GridPerson.svelte';
 	import Header from '$lib/sections/Header.svelte';
 	import Section from '$lib/sections/Section.svelte';
 
 	import H1 from '$lib/text/H1.svelte';
-	import H2 from '$lib/text/H2.svelte';
 	import type { Member } from '$lib/types';
 
 	let { data } = $props();
@@ -23,7 +21,4 @@
 			<Person title={name} {slug}></Person>
 		{/each}
 	</GridPerson>
-
-	<H2 color="dark">Past theses</H2>
-	<AlumniTable />
 </Section>
