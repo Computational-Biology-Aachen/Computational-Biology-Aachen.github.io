0cdebb9d0dc5970fba742007095a639a78a5f0af
Marvin van Aalst <marvin.vanaalst@gmail.com>
2025-10-06 11:58:49 +0200

    dev: new: contributor map

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -7,12 +7,18 @@
 	import H1 from '$lib/text/H1.svelte';
 	import type { Member } from '$lib/types';
 
+	import imgImport from '$lib/assets/contributor-map.png';
+	import Image from '$lib/Image.svelte';
+	import H2 from '$lib/text/H2.svelte';
+	import Text from '$lib/text/Text.svelte';
+
 	let { data } = $props();
 	let members: Member[] = data.members;
 </script>
 
 <Header>
 	<H1 color="light">Team</H1>
+	<Text color="light">Members of the AG Matuszyńska as of October 2025</Text>
 </Header>
 
 <Section color="light">
@@ -21,4 +27,10 @@
 			<Person title={name} {slug}></Person>
 		{/each}
 	</GridPerson>
+</Section>
+
+<Section color="light">
+	<H2>Where we come from</H2>
+	<Text>Celebrating our diverse backgrounds</Text>
+	<Image src={imgImport}></Image>
 </Section>
