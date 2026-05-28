a6a1ff5a9394a9273c5960023d4f79094f0039e2
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-05-28 10:08:53 +0200

    fix: url config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,7 @@
-import { dev } from '$app/environment'
+import { dev } from "$app/environment";
 
-export const title = 'The Matuszyńska Lab'
-export const description = 'Computational Biology lab at RWTH Aachen'
-export const url = dev ? 'http://localhost:5173/' : 'https://computational-biology-aachen.github.io/'
+export const title = "The Matuszyńska Lab";
+export const description = "Computational Biology lab at RWTH Aachen";
+export const url = dev
+  ? "http://localhost:5173/"
+  : "https://computational-biology-aachen.github.io/";
